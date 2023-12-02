0101722416c1176d0db388028a878673b483a890
Chris Dillon <squarism@gmail.com>
2023-12-01 20:38:45 -0800

    Add completions

---
+++ completions/lockrebase.fish
@@ -0,0 +1,15 @@
+# autocompletions for the package manager main argument
+complete -f -c lockrebase -n "__fish_use_subcommand" -a bundler -d "Resolve Gemfile.lock"
+complete -f -c lockrebase -n "__fish_use_subcommand" -a npm -d "Resolve package-lock.json"
+complete -f -c lockrebase -n "__fish_use_subcommand" -a poetry -d "Resolve poetry.lock"
+complete -f -c lockrebase -n "__fish_use_subcommand" -a yarn -d "Resolve yarn.lock"
+
+# autocomplete for --print-only flag
+complete -f -c lockrebase -l print-only -d "Prints the command to be run but does not execute it"
+
+# autocomplete for --main flag
+# complete -c lockrebase -l main -d "Overrides the remote branch. Default: origin/main"
+complete -f -c lockrebase -l main -d "Overrides the remote branch. Default: origin/main" -xa "(__fish_no_arguments)"
+
+# autocomplete for --help flag
+complete -f -c lockrebase -l help -d "Show help information"
